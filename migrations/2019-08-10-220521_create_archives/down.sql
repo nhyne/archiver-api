14fbd56fe44a343fde590b0cd4889f6682bf3d1a
nhyne <nhyne@nhyne.dev>
2019-09-21 11:45:14 -0400

    Revert "Revert "working towards standard endpoints""
    This reverts commit 2e588097b0133cc69f276f796f187f4ecf305ae9.

--- migrations/2019-08-10-220521_create_archives/down.sql
+++ migrations/2019-08-10-220521_create_archives/down.sql
@@ -1,2 +1,4 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE archives;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
--- migrations/2019-08-10-220521_create_archives/up.sql
+++ migrations/2019-08-10-220521_create_archives/up.sql
@@ -1,6 +1,8 @@
 -- Your SQL goes here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE archives (
-  id UUID PRIMARY KEY,
+  id UUID PRIMARY KEY default uuid_generate_v4(),
   original_link VARCHAR NOT NULL,
   archive_timestamp TIMESTAMPTZ NOT NULL
 );
